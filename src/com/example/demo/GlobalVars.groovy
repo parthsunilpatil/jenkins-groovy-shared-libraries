d9d405d6d4aa82715929bb66e26f295c8729e448
parthsunilpatil <parth.patil@imaginea.com>
2019-11-04 22:19:52 +0530

    Removing Function to create Yaml from Map

--- src/com/example/demo/GlobalVars.groovy
+++ src/com/example/demo/GlobalVars.groovy
@@ -2,5 +2,28 @@
 package com.example.demo
 
 class GlobalVars {
-	static final String GLOBALVARS_DEFAULT_NAME = "||GlobalVars||DEFAULT_NAME||"
+    static final String GLOBALVARS_DEFAULT_NAME = "||GlobalVars||DEFAULT_NAME||"
+    static final String PODTEMPLATE_YAML = """
+    apiVersion: v1
+    kind: Pod
+    spec:
+      containers:
+      - name: maven
+        image: maven:alpine
+        command:
+        - cat
+        tty: true
+      - name: helm
+        image: parthpatil3110/k8s-helm:latest
+        command:
+        - cat
+        tty: true
+        volumeMounts:
+        - name: kubeconfig
+          mountPath: /root/.kube/config
+      volumes:
+      - name: kubeconfig
+        hostPath:
+          path: /c/Users/parthp/.kube/config
+    """
 }
