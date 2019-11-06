ea2ade329fdc21fd8d7d39e3cb2eb19517db0ccc
parthsunilpatil <parth.patil@imaginea.com>
2019-11-06 17:56:36 +0530

    Maven, Docker build and helm methods

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -10,6 +10,17 @@ def call(Map config) {
 				helm repo list
 			"""
 		}
-		sh "helm upgrade --install ${config.name} --namespace ${config.namespace} --set image.repository=${config.imageRegistry}/${config.imageRepository},image.tag=${config.imageTag} ${config.chartsRepositoryName}/${config.chartName}"
+
+		if(config.containsKey("values")) {
+			print "Getting src from ${config.values.gitRepository} which contains values file"
+			sh "git clone -b ${config.values.gitBranch} ${config.values.gitRepository} ."
+			sh "pwd; ls -ltr"
+			sh "helm upgrade --install ${config.name} --namespace ${config.namespace} -f ${config.values.file} ${config.chartsRepositoryName}/${config.chartName}"
+		} else if(config.containsKey("image")) {
+			sh "helm upgrade --install ${config.name} --namespace ${config.namespace} --set image.repository=${config.image.registry}/${config.image.repository},image.tag=${config.image.tag} ${config.chartsRepositoryName}/${config.chartName}"	
+		} else {
+			sh "helm upgrade --install ${config.name} --namespace ${config.namespace} ${config.chartsRepositoryName}/${config.chartName}"
+		}
+		
 	}
 }
