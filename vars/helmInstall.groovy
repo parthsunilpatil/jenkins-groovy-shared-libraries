4c06586e5ba0d4cce86bf3c312713309f1a2c0d2
parthsunilpatil <parth.patil@imaginea.com>
2019-11-06 21:19:27 +0530

    Maven, Docker build and helm methods

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -11,16 +11,22 @@ def call(Map config) {
 			"""
 		}
 
+		def helmCmd = "helm upgrade --install ${config.name} --namespace ${config.namespace}"
+
 		if(config.containsKey("values")) {
 			print "Getting src from ${config.values.gitRepository} which contains values file"
 			sh "git clone -b ${config.values.gitBranch} ${config.values.gitRepository} ."
 			sh "pwd; ls -ltr"
-			sh "helm upgrade --install ${config.name} --namespace ${config.namespace} -f ${config.values.file} ${config.chartsRepositoryName}/${config.chartName}"
-		} else if(config.containsKey("image")) {
-			sh "helm upgrade --install ${config.name} --namespace ${config.namespace} --set image.repository=${config.image.registry}/${config.image.repository},image.tag=${config.image.tag} ${config.chartsRepositoryName}/${config.chartName}"	
-		} else {
-			sh "helm upgrade --install ${config.name} --namespace ${config.namespace} ${config.chartsRepositoryName}/${config.chartName}"
+			helmCmd += " -f ${config.values.file}"
+		} else if(config.containsKey("overrides")) {
+			config.overrides.each {
+				helmCmd += " --set ${it}"
+			}
 		}
+
+		helmCmd += " ${config.chartsRepositoryName}/${config.chartName}"
+		print "Helm command = ${helmCmd}"
+		sh helmCmd
 		
 	}
 }
