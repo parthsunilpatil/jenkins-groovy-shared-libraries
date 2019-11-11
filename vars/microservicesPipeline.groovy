e84e3e275ea92e1f545f0d1ac4e542d8050bef31
parthsunilpatil <parth.patil@imaginea.com>
2019-11-11 15:53:22 +0530

    Maven, Docker build and helm methods

---
+++ vars/microservicesPipeline.groovy
@@ -0,0 +1,58 @@
+#!/usr/bin/env groovy
+
+def call(Map config) {
+	pipeline {
+		agent none
+
+		stages {
+			
+			stage('Build') {
+
+				agent {
+					kubernetes {
+						yaml GlobalVars.PODTEMPLATE_BUILD_YAML
+					}
+				}
+
+				steps {
+					container('git') {
+						sh 'git --version'
+					}
+					container('maven') {
+						sh 'mvn --version'
+					}
+					container('docker') {
+						sh 'docker --version'
+					}
+				}
+
+			}
+
+			stage('Promotion') {
+				steps {
+					echo "Promotion"
+				}
+			}
+
+			stage('Deploy') {
+
+				agent {
+					kubernetes {
+						yaml GlobalVars.PODTEMPLATE_DEPLOY_YAML
+					}
+				}
+
+				steps {
+					container('helm') {
+						sh 'helm version'
+					}
+					container('kubectl') {
+						sh 'kubectl version'
+					}
+				}
+
+			}
+
+		}
+	}
+} 
