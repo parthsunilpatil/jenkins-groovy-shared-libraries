9158a3ee63aa095516863d7176a83c041ad5cd5c
parthsunilpatil <parth.patil@imaginea.com>
2020-01-13 18:14:49 +0530

    Fixing Issues

--- src/com/example/demo/Utilities.groovy
+++ src/com/example/demo/Utilities.groovy
@@ -3,18 +3,18 @@ package com.example.demo
 
 class Utilities {
 
-	static def notifyBuild(script, config, env) {
+	static def emailNotification(script, config) {
 	  // build status of null means successful
 	  def buildStatus = config.status ?: 'SUCCESSFUL'
-	  def subject = "${buildStatus} -> ${config.stage}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"  
+	  def subject = "${buildStatus} -> ${config.stage}: Job '${script.env.JOB_NAME} [${script.env.BUILD_NUMBER}]'"  
 	  def details = """<p>Hi Team,</p>
 	  <p><b><u>Jenkins Build Pipeline Information</u></b></p>
-	  <p>Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+	  <p>Job '${script.env.JOB_NAME} [${script.env.BUILD_NUMBER}]':</p>
 	  <p>Stage: ${config.stage} -> Status: ${buildStatus}</p>
-	  <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
+	  <p>Check console output at "<a href="${script.env.BUILD_URL}">${script.env.JOB_NAME} [${script.env.BUILD_NUMBER}]</a>"</p>
 	  <p>Regards,</p>
 	  <p>Jenkins</p>"""
-	  emailext (
+	  script.emailext (
 	    mimeType: 'text/html',
 	    to: config.recipients,
 	    subject: subject,
