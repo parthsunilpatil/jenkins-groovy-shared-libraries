8cf1c29c53e7b142bdb6c41bd116838a65b5a461
parthsunilpatil <parth.patil@imaginea.com>
2020-04-11 13:36:44 +0530

    Added basic1d20GamePipeline and PipelineWrappers

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -4,27 +4,61 @@ import com.example.demo.PipelineWrappers
 import com.example.demo.YamlPodConfigurationBuilder
 
 def call(Map config) {
-	stage("1") {
-		node("slave-1") {
-			stage("1.1") {
-				echo "1.1"
-			}
-			stage("1.2") {
-				echo "1.2"
-			}
-		}
-	}
+	pipeline {
+		agent none
+		
+		stages {
+			
+			stage("1") {
 
-	config.iterations.each {
-		stage("${it}") {
-			node("slave-1") {
-				stage("${it}.1") {
-					echo "${it}.1"
+				agent {
+					label "slave-1"
 				}
-				stage("${it}.2") {
-					echo "${it}.2"
+
+				stages {
+					stage("1.1") {
+			            steps {
+			                echo "1.1"
+			            }
+			        }
+
+			        stage("1.2") {
+			            steps {
+			                echo "1.2"
+			            }
+			        }
 				}
 			}
+
+			stage("2") {
+
+				agent {
+					label "slave-1"
+				}
+
+				stages {
+					stage("2.1") {
+			            steps {
+			                echo "2.1"
+			            }
+			        }
+
+			        stage("2.2") {
+			            steps {
+			                echo "2.2"
+			            }
+			        }
+				}
+			}
+
+			post {
+				always {
+					node("slave-1") {
+						cleanWs()
+					}
+				}
+			}
+
 		}
 	}
 }
