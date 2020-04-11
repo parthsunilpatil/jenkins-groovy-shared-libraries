50be54a597eb2c97c4f846825f203cc31e3b7460
parthsunilpatil <parth.patil@imaginea.com>
2020-04-11 13:06:57 +0530

    Added basic1d20GamePipeline and PipelineWrappers

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -1,20 +1,25 @@
 #!/usr/bin/env groovy
 import com.example.demo.PodTemplateYamls
+import com.example.demo.PipelineWrappers
 import com.example.demo.YamlPodConfigurationBuilder
 
 def call(Map config) {
 	pipeline {
 		agent none
-		stages {
-			stage("Test") {
+		
+		PipelineWrappers.stages(this, [
+			agent: [label: "slave-1"],
+			iterations: [
+				[iteration: "1", value: "one"],
+				[iteration: "2", value: "two"],
+				[iteration: "3", value: "three"]
+			]
+		], { closureParams ->
+			stage("${closureParams.iteration}") {
 				steps {
-					script {
-						echo config.welcomeMessage
-						echo new YamlPodConfigurationBuilder().withDefaultBuildStages().build()
-						echo new YamlPodConfigurationBuilder().withDefaultDeployStages().build()
-					}
+					echo "${closureParams.value}"
 				}
 			}
-		}
+		})
 	}
 }
