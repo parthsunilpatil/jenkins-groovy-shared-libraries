7ee032494dbac3c5c7aa62ca33317216aa2e835b
parthsunilpatil <parth.patil@imaginea.com>
2020-04-17 19:59:39 +0530

    Added basic1d20GamePipeline and PipelineWrappers

--- src/com/example/demo/microservices/MicroservicesPipelineStages.groovy
+++ src/com/example/demo/microservices/MicroservicesPipelineStages.groovy
@@ -7,9 +7,25 @@ class MicroservicesPipelineStages extends PipelineStages {
 		super(script)
 	}
 
+	def checkout() {
+		script.sh script: """
+			git clone -b ${script.params.GIT_BRANCH} ${script.params.GIT_REPOSITORY} .
+			pwd; ls -ltr
+		""", label: "Checkout Source Code"
+	}
+
+	def build() {
+		script.sh script: """
+			mvn clean install
+		""", label: "Maven Build"
+	}
+
 	def distribute(config) {
 		def dockerTag = script.readMavenPom().properties["docker-tag-version"]
-		super.distribute(config: [dockerTag: dockerTag])
+		
+		script.sh script: """
+			docker build -t ${script.params.DOCKER_REGISTRY}/${script.params.DOCKER_REPOSITORY}:${dockerTag} .
+		""", label: "Docker Build & Deploy"
 	}
 
 }
--- src/com/example/demo/microservices/PipelineStages.groovy
+++ src/com/example/demo/microservices/PipelineStages.groovy
@@ -3,31 +3,12 @@ package com.example.demo.microservices
 
 import com.example.demo.PipelineWrappers
 
-class PipelineStages {
+abstract class PipelineStages {
 
 	def script
 
 	PipelineStages(script) {
 		this.script = script
-	}
-
-	def checkout() {
-		script.sh script: """
-			git clone -b ${script.params.GIT_BRANCH} ${script.params.GIT_REPOSITORY} .
-			pwd; ls -ltr
-		""", label: "Checkout Source Code"
-	}
-
-	def build() {
-		script.sh script: """
-			mvn clean install
-		""", label: "Maven Build"
-	}
-
-	def distribute(config) {
-		script.sh script: """
-			docker build -t ${script.params.DOCKER_REGISTRY}/${script.params.DOCKER_REPOSITORY}:${config.dockerTag} .
-		""", label: "Docker Build & Deploy"
 	}
 
 	def dynamicAgentBuildStages(config) {
@@ -50,7 +31,7 @@ class PipelineStages {
 
 				script.stage("distribute") {
 					script.container(config.distribute.container) {
-						distribute(config.distribute.config)
+						distribute(config.distribute.dockerTag)
 					}
 				}
 			}
