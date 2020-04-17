6507e8b92e08fa93d746ffecc5f27cec05553b83
parthsunilpatil <parth.patil@imaginea.com>
2020-04-17 20:19:44 +0530

    Added basic1d20GamePipeline and PipelineWrappers

---
+++ src/com/example/demo/microservices/AIMicroservicesPipelineStages.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+package com.example.demo.microservices
+
+import com.example.demo.microservices.MicroservicesPipelineStages
+
+class AIMicroservicesPipelineStages extends MicroservicesPipelineStages {
+
+	AIMicroservicesPipelineStages(script) {
+		super(script)
+	}
+
+}
