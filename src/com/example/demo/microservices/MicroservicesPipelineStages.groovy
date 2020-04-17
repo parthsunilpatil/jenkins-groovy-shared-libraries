480340e4c550b20229b13230920cb4b18cdc1dc4
parthsunilpatil <parth.patil@imaginea.com>
2020-04-17 18:55:19 +0530

    Added basic1d20GamePipeline and PipelineWrappers

--- src/com/example/demo/microservices/MicroservicesPipelineStages.groovy
+++ src/com/example/demo/microservices/MicroservicesPipelineStages.groovy
@@ -3,9 +3,9 @@ package com.example.demo.microservices
 
 class MicroservicesPipelineStages implements PipelineStages {
 
-	def distribute(config) {
+	/*def distribute(config) {
 		def dockerTag = script.readMavenPom().properties["docker-tag-version"]
 		PipelineStages.super.distribute(config: [dockerTag: dockerTag])
-	}
+	}*/
 
 }
